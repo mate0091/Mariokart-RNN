f2be4447e406b327e112596a4ce9b6d2cc954adc
mate0091 <mate.kelemen009@gmail.com>
2020-04-19 19:36:25 +0300

    initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import CuDNNLSTM
+import ctypes
+
+if __name__ == '__main__':
+    print(tf.version)
+
+    #mnist = tf.keras.datasets.mnist
+
+    #(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    
+
